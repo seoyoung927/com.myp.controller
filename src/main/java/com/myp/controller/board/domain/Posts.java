dfa701202cefcd08a778b860f412a86098f3c47b
Choseoyoung <seoyoung0000@naver.com>
2023-01-11 21:54:19 +0900

    fix: mysql error

--- src/main/java/com/myp/controller/board/domain/Posts.java
+++ src/main/java/com/myp/controller/board/domain/Posts.java
@@ -1,9 +1,6 @@
 package com.myp.controller.board.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
